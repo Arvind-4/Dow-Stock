5203bbbfdfe0f7dd9729bfa5aa909679d9fec773
Arvind-4 <lordarvindthegamer@gmail.com>
2023-05-20 15:01:19 +0530

    Updated: app files

--- app/main.py
+++ app/main.py
@@ -1,28 +1,42 @@
-import json
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response, status
+from fastapi.middleware.cors import CORSMiddleware
 
 from .stocks import get_dow30_data
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/api")
-async def index():
+async def index(request: Request, response: Response):
     return {"message": "Hello World", "status": True, "data": {}}
 
 
 @app.get("/api/stocks")
-async def getStocks(start: Optional[str] = None, end: Optional[str] = None):
+async def getStocks(
+    request: Request,
+    response: Response,
+    start: Optional[str] = None,
+    end: Optional[str] = None,
+):
     if start is None or end is None:
+        response.status_code = status.HTTP_404_NOT_FOUND
         return {"message": "start and end is required", "status": False, "data": {}}
-    data = get_dow30_data(
-        "-".join(start.split("-")[::-1]), "-".join(end.split("-")[::-1])
-    )
+    data = get_dow30_data(start, end)
     if data == {}:
         return {"message": "Data not found", "status": False, "data": {}}
     return {
         "message": "Data retrieved successfully",
         "status": True,
-        "data": json.loads(data),
+        "data": data,
     }
--- app/stocks.py
+++ app/stocks.py
@@ -3,7 +3,7 @@ import yfinance as yf
 
 def get_dow30_data(start_date, end_date):
     data = yf.download("^DJI", start=start_date, end=end_date, interval="1d")
-    data = data["Adj Close"].to_json(orient="index", date_format="iso")
-    if data == "{}":
+    data = data["Adj Close"].to_dict()
+    if len(data) == 0 or data is None or data == {} or data == dict():
         return {}
     return data
