2b3a48801022a0a2bd03362e489156796605fb04
Arvind-4 <lordarvindthegamer@gmail.com>
2023-05-20 10:40:26 +0530

    Added & Updated: for next app

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <p className="text-red-800">Hello World</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+      </p>
+    </div>
+  )
+}
